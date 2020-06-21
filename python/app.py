efd2002db008f15c957e634228b04b2d2a607afb
Adrian Agnic <adrian.agnic@hotmail.com>
2020-06-21 01:10:30 -0400

    start Flask server

--- python/app.py
+++ python/app.py
@@ -1,8 +1,16 @@
+from flask import Flask
+
 import storage.dummy_data
 from storage.mongo import MongoDB
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello, world.'
+
 
 if __name__ == '__main__':
-    conn_str = "mongodb://admin:admin@db:27017"
-
     client = MongoDB()
+    app.run(debug=True, host='0.0.0.0', port='5001')
